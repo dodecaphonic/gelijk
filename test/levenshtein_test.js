eacf32da053c6f78d73a6807eb0c604d748f2918
Vitor Capela <dodecaphonic@gmail.com>
2016-08-10 18:48:16 -0300

    Extract the common test-suite

--- test/levenshtein_test.js
+++ test/levenshtein_test.js
@@ -2,21 +2,30 @@ const assert = require("assert");
 const jsc = require("jsverify");
 const Lev = require("../src/gelijk/levenshtein");
 
+const suite = (distance) => {
+  return () => {
+    it("defaults to the other String's length if either String is empty", () => {
+      assert(distance("", "flag") === 4);
+      assert(distance("drag", "") === 4);
+    });
+
+    jsc.property("is always 0 when String is compared to itself", "string", (word) => (
+      distance(word, word) === 0
+    ));
+
+    it("is the edit distance between both Strings when they're different", () => {
+      assert(distance("quota", "quote") === 1);
+      assert(distance("cling", "bring") === 2);
+      assert(distance("clue", "recluse") === 3);
+    });
+  };
+};
+
 describe("The distance between two Strings", () => {
-  it("defaults to the other String's length if either String is empty", () => {
-    assert(Lev.levenshtein("", "flag") === 4);
-    assert(Lev.levenshtein("drag", "") === 4);
-  });
+  describe("calculated using a naive, recursive, Levenshtein implementation",
+           suite(Lev.naiveLevenshtein));
 
-  jsc.property("is always 0 when String is compared to itself", "string", (word) => (
-    Lev.levenshtein(word, word) === 0
-  ));
-
-  it("is the edit distance between both Strings when they're different", () => {
-    assert(Lev.levenshtein("quota", "quote") === 1);
-    assert(Lev.levenshtein("cling", "bring") === 2);
-    assert(Lev.levenshtein("clue", "recluse") === 3);
-  });
+  describe("calculated using an implementation of Wagner-Fischer", suite(Lev.levenshtein));
 
   jsc.property("faster implementation is consistent with the naive implementation", "string", "string", (a, b) => (
     Lev.levenshtein(a, b) === Lev.naiveLevenshtein(a, b)
