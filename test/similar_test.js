4263b35f8a060e014e97c4a409fd2392fef546eb
Vitor Capela <dodecaphonic@gmail.com>
2016-08-11 10:14:09 -0300

    Test similarity with props instead of examples

--- test/similar_test.js
+++ test/similar_test.js
@@ -1,33 +1,30 @@
 const assert = require("assert");
 const jsc = require("jsverify");
+const { map } = require("ramda");
 
 const areSimilar = require("../src/gelijk/similar");
 
+const shiftCharsByOne = map((c) => String.fromCharCode(c.charCodeAt(0) + 1));
+
 describe("the similarity between two words", () => {
-  it("is inexistent if the size difference between them is larger than threshold", () => {
-    assert(!areSimilar("gala", "gallantry", 3));
-    assert(!areSimilar("gala", "gallantry", 4));
-  });
+  jsc.property("is inexistent if the size difference between them is larger than threshold",
+               "nestring", "nestring",
+               (a, b) => !areSimilar(a, b, Math.abs(b.length - a.length) - 1));
 
-  it("is inexistent if the edit distance is larger than the threshold", () => {
-    assert(!areSimilar("gala", "home", 3));
-    assert(!areSimilar("cling", "quite", 3));
-  });
+  jsc.property("is inexistent if the edit distance is larger than the threshold",
+               "nestring",
+               (word) => !areSimilar(word, shiftCharsByOne(word), word.length - 1));
 
-  it("can be attested if the edit distance is at most as large as the threshold", () => {
-    assert(areSimilar("gala", "gale", 3));
-    assert(areSimilar("gala", "male", 3));
-    assert(areSimilar("morgue", "corgi", 3));
-    assert(areSimilar("a", "aces", 3));
-  });
+  jsc.property("can be attested if the edit distance is at most as large as the threshold",
+               "nestring",
+               (word) => areSimilar(word, shiftCharsByOne(word), word.length));
+
+  jsc.property("exists if both words are equal",
+               "string",
+               (word) => areSimilar(word, word, 0));
 
   it("cannot be attested if either word is empty", () => {
     assert(!areSimilar("", "loneliness", 30));
     assert(!areSimilar("loneliness", "", 42));
-  });
-
-  it("exists if both words are equal", () => {
-    assert(areSimilar("word", "word", 0));
-    assert(areSimilar("", "", 0));
   });
 });
