cca00b0e88bdbdb24bf465be75f43224b90c88fd
Vitor Capela <dodecaphonic@gmail.com>
2016-08-12 12:17:35 -0300

    Add documentation to the BKTree's public interface

--- src/gelijk/bk_tree.js
+++ src/gelijk/bk_tree.js
@@ -1,9 +1,37 @@
+/**
+ * A Burkhard-Teller tree is a data structure that allows for fast
+ * searching of proximate strings, using the edit distance between its
+ * nodes as the branching criterion. A best-case search should take
+ * O(lg n) time, but lower thresholds (that is, maximum edit
+ * distances) can yield very short tree hops before finding all
+ * results. For more on that, see
+ * https://github.com/vy/bk-tree#performance.
+ *
+ * The tree is implemented as a purely functional datastructure, which
+ * means every change to its contents yields a new tree. It doesn't,
+ * however, copy every single value -- it shares most of the structure
+ * and changes only the affected node.
+ *
+ * @example
+ * const tree = createTree("word");
+ * const updatedTreeA = addWord(tree, "work");
+ * const updatedTreeB = addWord(updatedTreeA, "core");
+ * searchWords(updatedTreeB, 2, "wo") // => ["word", work"]
+ * searchWords(updatedTreeB, 1, "more") // => ["core"]
+ */
 const R = require("ramda");
 
 const { levenshtein } = require("./levenshtein");
 
+
 const children_ = R.lensProp("children");
 
+/**
+ * Creates a new BK Tree.
+ *
+ * @param {string} word - A word
+ * @return {Object} the tree node
+ */
 const createTree = (word) => {
   return {
     word: word.toLowerCase(),
@@ -11,6 +39,13 @@ const createTree = (word) => {
   };
 };
 
+/**
+ * Adds a word to the BK Tree.
+ *
+ * @param {Object} root - The tree root
+ * @param {string} word - The new word
+ * @return {Object} the updated tree
+ */
 const addWord = (root, word) => {
   const normalizedWord = word.toLowerCase();
 
@@ -40,7 +75,13 @@ const addChild = (node, word, dist) => (
         node)
 );
 
-const allWords = (tree) => {
+/**
+ * Builds a list of every word that's in the tree.
+ *
+ * @param {Object} root - the tree root
+ * @return {Array.<string>} an array of words
+ */
+const allWords = (root) => {
   const buildList = (words, node) => {
     if (isLeaf(node)) {
       return words.concat(node.word);
@@ -50,11 +91,20 @@ const allWords = (tree) => {
     }
   };
 
-  return buildList([], tree);
+  return buildList([], root);
 };
 
 const second = R.view(R.lensIndex(1));
 
+/**
+ * Searches for words matching a reference word, with the expectation
+ * that they are at most _threshold_ edits away from it.
+ *
+ * @param {Object} tree - the tree root
+ * @param {number} threshold - the maximum edit distance
+ * @param {string} word - the word one is searching for
+ * @return {Array.<string>} all words matching the criteria
+ */
 const searchWords = R.curry((tree, threshold, word) => {
   const normalizedWord = word.toLowerCase();
 
